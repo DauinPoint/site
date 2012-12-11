a86d6d7e430da5c6bfebc8cb2175ee967c83ae83
Gavin Hughes <gavhug@gmail.com>
2012-12-11 10:44:16 +0800

    Add Dauin Point logo and styling.

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 require 'slim'
+require 'sass'
+
+SASS_DIR = File.expand_path("../stylesheets", __FILE__)
 
 get '/' do
   slim :index
+end
+
+get "/stylesheets/:stylesheet.css" do |stylesheet|
+  content_type "text/css"
+  template = File.read(File.join(SASS_DIR, "#{stylesheet}.sass"))
+  Sass::Engine.new(template).render
 end
